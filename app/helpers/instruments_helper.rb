1a0e39d38e71e5cdef9debe17f1c6d211fad3165
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 13:01:50 -0400

    adds instrument helpers

--- app/helpers/instruments_helper.rb
+++ app/helpers/instruments_helper.rb
@@ -1,2 +1,17 @@
 module InstrumentsHelper
+  def self.family
+    self.category.family.name
+  end
+
+  def banjo_categories
+    Category.where(id: [1...5])
+  end
+
+  def guitar_categories
+    Category.where(id: [5...13])
+  end
+
+  def mandolin_categories
+    Category.where(id: [13...16])
+  end
 end
