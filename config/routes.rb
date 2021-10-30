0248916a3e8b1bcf8fafbe7b362f309e88ddf5af
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 13:03:12 -0400

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
-  resources :instruments
-  resources :users
-  resources :makers
-  resources :categories
-  resources :families
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  get '/signup', to: "users#new" # note: '/signup' = signup_path
+  post '/users', to: "users#create"
+  
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users, except: [:new, :create]
+
+  resources :families, only: [:show] do
+    resources :instruments, only: [:new]
+    resources :categories, only: [:index]
+
+  end
+
+  resources :families, only: [:index]
+  # resources :categories, only: [:index, :show]
+  resources :makers, only: [:index, :show]
+  resources :instruments, except: [:new]
+
+  get '/auth/facebook/callback', to: 'sessions#create_with_fb'
+
+  end 
