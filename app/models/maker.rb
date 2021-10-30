287e683ab0498cdf126c2b51637f0393d33ef59e
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 12:56:17 -0400

    fills out maker associations

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -1,2 +1,6 @@
 class Maker < ApplicationRecord
+  has_many :instruments
+  has_many :users, through: :instruments
+  has_many :categories, through: :instruments
+  has_many :families, through: :instruments
 end
