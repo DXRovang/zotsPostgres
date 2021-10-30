64b4c1d0553801f68930c122f8c4b99a7d3d5fdf
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 18:39:26 -0400

    fills in maker controller

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,2 +1,14 @@
 class MakersController < ApplicationController
+  
+  def index
+    if params[:search]
+      @makers = Maker.search(params[:search])
+    end
+  end
+
+  def show
+    #currently unused if no instruments have this maker
+    @maker = Maker.find_by(id: params[:id])
+  end
+
 end
