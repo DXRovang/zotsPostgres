6182831ab5289e0b781ab5144d82ffadf482288e
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 12:57:42 -0400

    adds family associations

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,2 +1,6 @@
 class Family < ApplicationRecord
+  has_many :instruments
+  has_many :users, through: :instruments
+  has_many :makers, through: :instruments
+  has_many :categories, through: :instruments
 end
