e23c0d910a681c8f100d0e48961aa9a470b26dd2
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 18:55:00 -0400

    fills in helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  #using helper_method allows current_user to be used in views
+  helper_method :current_user
+
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    redirect_to "/login" if !current_user
+  end
 end
+#testing
