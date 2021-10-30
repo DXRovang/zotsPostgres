ddf7717d5cf4936c83eaff1f356fa09bf02b606f
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 12:55:36 -0400

    fills out user model associations/validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  include ActiveModel::Validations
+  has_secure_password
+
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email
+  validates_with EmailValidator
+  
+  has_many :instruments
+  has_many :categories, through: :instruments
+  has_many :makers, through: :instruments
+  has_many :families, through: :instruments
 end
