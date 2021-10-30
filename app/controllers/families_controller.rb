f1002680e782c479cc1a23365ab9f3e64641496c
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 18:38:55 -0400

    fills in family controller

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,12 @@
 class FamiliesController < ApplicationController
+  before_action(:require_login)
+  
+  def index
+    @families = Family.all
+  end
+
+  def show
+    #currently unused
+    @family = Family.find_by(id: params[:id])
+  end
 end
