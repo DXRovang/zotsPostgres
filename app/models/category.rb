d5991ebc0406039f02091c4eee2104c5b88e6771
Alexander Rovang <DXRovang@gmail.com>
2021-10-30 12:58:13 -0400

    adds category associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :instruments
+  has_many :users, through: :instruments
+  has_many :makers, through: :instruments
+  has_many :families, through: :instruments
 end
