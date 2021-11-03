0ffd93d7e498022e51cae0c67a24d1e8241d99f9
Alexander Rovang <DXRovang@gmail.com>
2021-11-03 14:15:06 -0400

    resets port in puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,12 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+
+const PORT = process.env.PORT || '8080'
+
+port ENV.fetch("PORT") { PORT }
+
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
