4f60266ed459df287f1be87ab99f023e65841466
Alexander Rovang <DXRovang@gmail.com>
2021-11-04 15:00:45 -0400

    changes family name to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_164717) do
+ActiveRecord::Schema.define(version: 2021_11_04_185701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_10_30_164717) do
   end
 
   create_table "families", force: :cascade do |t|
-    t.string "name"
+    t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
